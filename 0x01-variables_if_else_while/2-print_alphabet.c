2c15ea1a959c2c041ec5d38b70a62df4f2cd4dfb
owanikin <oderindeife@gmail.com>
2023-02-17 15:28:40 -0800

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower letter
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= '2'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
