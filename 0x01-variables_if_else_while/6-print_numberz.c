61fa2827d5ac86af673dc4516e87b2671c5a08fd
owanikin <oderindeife@gmail.com>
2023-02-17 16:00:37 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit in base 10
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0'));
+
+	putchar('\n');
+
+	return (0);
+}
