b7e1582f3edd7355f4d19fc3337c0a723cc23498
owanikin <oderindeife@gmail.com>
2023-02-21 21:59:02 -0800

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: th first number
+ * @b: the second number
+ * Return: the addition of both
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
