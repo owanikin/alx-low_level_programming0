211ec9bd6b2cf9d11a4dc9727f730a0b2796ebea
owanikin <oderindeife@gmail.com>
2023-02-23 19:49:37 -0800

    prints numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 - 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar(c);
+}
